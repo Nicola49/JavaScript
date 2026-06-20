e92a5aa322b842918d7e59db8baa3eb365863088
Carlos Nicola <canicola2007@gmail.com>
2026-06-20 13:59:48 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 60
+ 
+
+console.log(`A velocidade do seu carro é de ${vel} km/h.`)
+
+if (vel > 50) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+} else {
+    console.log("Você está dentro do limite de velocidade. Continue assim!")
+}
+
+console.log("Dirija sempre com cinto de segurança!")
